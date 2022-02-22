111dc4393951162c37f5416f9165c06b18129004
Loic-Delbarre <Zhf3PzewgSoGtzlofybR@protonmail.com>
2022-02-22 09:57:23 +0100

    correction de complexité absude

--- fun.py
+++ fun.py
@@ -2,13 +2,12 @@ import random
 import numpy as np 
 from numpy.polynomial import Polynomial as poly
 import sys
-def ction(size):
+def ction(size=4):
     if(size<= 1):
         print("You must include at least 1 argument(number of random array)")
         return -1
-    length= random.randrange(2,15,1)
-    coef=np.random.rand(1,length)
+    coef=np.random.rand(1,size)
     coef=coef.squeeze()
     return poly(coef)
 
-
+print(ction())
